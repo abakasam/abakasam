f5d8da3572458cd6040c26975bf360cbe525baa9
Sam <abakasam@outlook.com>
2021-07-19 02:31:06 +0000

    hello

--- environment-setup/setup-environment.sh
+++ environment-setup/setup-environment.sh
@@ -11,16 +11,14 @@ cd $directoryworking
 git clone $userURL
 git clone $projectURL
 
-exit
-
 packagenode="v0.12.18"
 wget https://nodejs.org/dist/$packagenode/node-$packagenode.tar.gz
 tar -xf node-$packagenode.tar.gz
 mv node-$packagenode.tar.gz node-$packagenode
 
-
-export PYTHON="${directoryuser}/environment-setup/Python-2.7.9/python"
-$PYTHON ./node-v0.12.18/configure --without-snapshot
-cd $directoryuser/environment-setup/node-v0.12.18/
+packagepython="2.7.9"
+export PYTHON="${directoryuser}/environment-setup/Python-$packagepython/python"
+$PYTHON ./node-$packagenode/configure --without-snapshot
+cd $directoryuser/environment-setup/node-$packagenode/
 make
 make install
